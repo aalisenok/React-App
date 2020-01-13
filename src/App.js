b04231c594b3703fc80dbfa279edd2675be46231
Andrey Alisenak <aalisenak@gmail.com>
2020-01-14 02:12:52 +0300

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Layout from "./hoc/Layout/Layout";
+import {Route, Switch} from 'react-router-dom'
 import Quiz from "./containers/Quiz/Quiz";
+import QuizCreator from "./containers/QuizCreator/QuizCreator";
+import QuizList from "./containers/QuizList/QuizList";
+import Auth from "./containers/Auth/Auth";
 
 function App() {
   return (
     <Layout>
-        <Quiz>
-        </Quiz>
+        <Switch>
+        <Route path='/auth' component={Auth}/>
+        <Route path='/quize-creator' component={QuizCreator}/>
+        <Route path='/quize/:id' component={Quiz}/>
+        <Route path='/' component={QuizList}/>
+
+        </Switch>
     </Layout>
   );
 }
---
+++ src/containers/QuizCreator/QuizCreator.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default  class QuizCreator extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>QuizCreator</h1>
+            </div>
+        );
+    }
+}
+
